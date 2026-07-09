6165c6cec2ec842a9b1a7faf96f63600b7437b4e
farhihind02-pixel <farhihind02@gmail.com>
2026-07-09 13:52:26 +0100

    Update project

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ const {
 } = process.env;
 
 const ACC_FOLDER_URN = 'urn:adsk.wipprod:fs.folder:co.y57lR8imTbuJh37gU440fA';
-const VERSION_URN = 'urn:adsk.wipprod:fs.file:vf.Fs-fmn5sROy4n6m4S5jokA?version=34';
+const VERSION_URN = 'urn:adsk.wipprod:fs.file:vf.Fs-fmn5sROy4n6m4S5jokA?version=35';
 const VIEWABLE_GUID  = '40d54ded-3c29-f5a3-ed21-dc3126f84375';
 
 const DERIVATIVE_URN = Buffer.from(VERSION_URN).toString('base64')
